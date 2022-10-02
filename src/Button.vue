3a8bed5f398d9001f3c7849fc784d491bbd4c8ca
BarryDong <286084845@qq.com>
2022-10-02 16:51:21 +0800

    Button 组件支持传入 iconPosition 属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,9 +1,11 @@
 <template>
-  <button class="s-button">
+  <button :class="['s-button', {[`icon-${iconPosition}`]: icon}]">
     <svg v-if="icon" class="icon" aria-hidden="true">
       <use :xlink:href="`#i-${icon}`"></use>
     </svg>
-    <slot></slot>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
@@ -11,7 +13,8 @@
 export default {
   name: 'Button',
   props: {
-    icon: { type: String }
+    icon: { type: String },
+    iconPosition: { type: String, default: 'left' }
   }
 }
 </script>
@@ -22,9 +25,19 @@ export default {
   height: var(--button-height);
   background: var(--button-bg);
   border: 1px solid var(--border-color); border-radius: var(--border-radius);
+  display: inline-flex; align-items: center;
+  vertical-align: middle;
   padding: 0 1em;
   &:hover { border-color: var(--border-color-hover); }
   &:active { background: var(--button-bg-active); }
   &:focus { outline: none; }
+  &.icon-left {
+    > .icon { order: 1; margin-right: .2em; }
+    > .content { order: 2; }
+  }
+  &.icon-right {
+    > .icon { order: 2; margin-left: .2em; margin-right: 0; }
+    > .content { order: 1; }
+  }
 }
 </style>
