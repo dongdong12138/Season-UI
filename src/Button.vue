1bd5a40a67ec1acffedd4bb7b85040c3802dbc1a
BarryDong <286084845@qq.com>
2022-10-02 16:32:23 +0800

    将 Button.js 重构为 Button.vue & 执行 npx parcel index.html --no-cache 页面报错

---
+++ src/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="s-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+
+<style scoped lang="scss">
+.s-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  background: var(--button-bg);
+  border: 1px solid var(--border-color); border-radius: var(--border-radius);
+  padding: 0 1em;
+  &:hover { border-color: var(--border-color-hover); }
+  &:active { background: var(--button-bg-active); }
+  &:focus { outline: none; }
+}
+</style>
